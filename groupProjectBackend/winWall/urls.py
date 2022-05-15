fade3a429f0ff19dbacd08a470098a9a981a1d7c
Kristie <kmlbrandis@gmail.com>
2022-05-15 18:15:17 +0800

    Sticky note POST function
    including view, url, model
    
    also added user serializer file (blank)

---
+++ groupProjectBackend/winWall/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+
+# sticky note url 
+
+urlpatterns = [
+    path('stickyNote/', views.StickyNote.as_view()),
+
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
