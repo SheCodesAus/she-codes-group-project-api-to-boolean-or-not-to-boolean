e71038da80fc8f8487eaf64552f90dfd9f90b4ab
HannahHollywood <70626629+HannahHollywood@users.noreply.github.com>
2022-05-19 21:02:33 +0800

    Added Admin module back-in to start auth_type stuff

--- groupProjectBackend/groupProjectBackend/settings.py
+++ groupProjectBackend/groupProjectBackend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
