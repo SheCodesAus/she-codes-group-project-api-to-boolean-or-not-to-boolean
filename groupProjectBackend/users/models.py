3ead44cc2312eebc1e7cdd71bb1b9f95eb9966ab
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-02-16 16:21:01 +0800

    trying to fix operational error

--- groupProjectBackend/users/models.py
+++ groupProjectBackend/users/models.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import AbstractUser 
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth import get_user_model
 from django.db import models
+
 
 class SheCodesUser(AbstractUser):
     avatar = models.URLField(null=True, blank=True)
@@ -13,4 +14,3 @@ class SheCodesUser(AbstractUser):
 
     def __str__(self):
         return self.username
-
